5049373d8570d788d4369e857b2e5940bdc74c92
pelinlimited <felinnn96@gmail.com>
2024-10-25 17:43:25 +0700

    LATIHAN_UTS
    commit 1

---
+++ LATIHAN_UTS/models/produk.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const produkSchema = new mongoose.Schema({
+    nama : {
+        type : String,
+    },
+    deskripsi : {
+        type : String,
+    },
+    harga : {
+        type : Number,
+    },
+    stok : {
+        type : Number,
+    },
+    kategori_id : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: 'Kategori',
+        required: true,
+    }
+});
+
+const Produk = mongoose.model('Produk', produkSchema);
+module.exports = Produk;
